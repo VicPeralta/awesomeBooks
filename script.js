00dbab098477e3c0cf140254fa8078c21f50f2f6
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-02-07 10:30:59 -0600

    Add js code to dynamically create book's card section.
    Add .vscode to .gitignore
    Add template tag to html code
    
    modified:   .gitignore
    modified:   index.html
    modified:   script.js

--- script.js
+++ script.js
@@ -2,27 +2,45 @@ const bookList = [
   {
     id: 0,
     title: 'Book 1',
-    author: 'Test test'
+    author: 'Test test',
   },
   {
     id: 1,
     title: 'Book 2',
-    author: 'Test test'
+    author: 'Test test',
   },
   {
     id: 2,
     title: 'Book 3',
-    author: 'Test test'
+    author: 'Test test',
   },
   {
     id: 3,
     title: 'Book 4',
-    author: 'Test test'
-  }
-]
+    author: 'Test test',
+  },
+];
 
 function displayBooks(bookList) {
-  bookList.forEach(book => {
-
+  const booksContainer = document.querySelector('.books-container');
+  const booksTemplate = document.getElementById('books-template');
+  bookList.forEach((book) => {
+    const bookCard = booksTemplate.content.cloneNode(true).children[0];
+    bookCard.querySelector('.title').textContent = book.title;
+    bookCard.querySelector('.author').textContent = book.author;
+    bookCard.querySelector('.remove-book').setAttribute('data-id', book.id);
+    booksContainer.appendChild(bookCard);
   });
 }
+
+function setListeners() {
+  document.addEventListener('click', (e) => { 
+    if(e.target.matches('.remove-book')) {
+      console.log(e.target.dataset.id);
+    }
+  });
+
+}
+
+displayBooks(bookList);
+setListeners();
