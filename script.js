f110cdb87ec2ef89bb9c6a4611fb6a8c20a092ee
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-02-07 10:10:14 -0600

    Add script file and booksList obj

---
+++ script.js
@@ -0,0 +1,28 @@
+const bookList = [
+  {
+    id: 0,
+    title: 'Book 1',
+    author: 'Test test'
+  },
+  {
+    id: 1,
+    title: 'Book 2',
+    author: 'Test test'
+  },
+  {
+    id: 2,
+    title: 'Book 3',
+    author: 'Test test'
+  },
+  {
+    id: 3,
+    title: 'Book 4',
+    author: 'Test test'
+  }
+]
+
+function displayBooks(bookList) {
+  bookList.forEach(book => {
+
+  });
+}
