5048991e60bfbbb474c8d9b2499e3303c7a1fc92
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-02-07 11:04:42 -0600

    Add book function and move template outside books-container

--- script.js
+++ script.js
@@ -50,6 +50,20 @@ function loadDataFromStorage() {
   return list;
 }
 
+function addBook(title, author, bookList) {
+  let index = 0;
+  
+  if (bookList.length !== 0) {
+    index = bookList.length;
+  }
+  
+  bookList.push({
+    id: index,
+    title: title,
+    author: author
+  });
+}
+
 function BookShelf() {
   this.bookList = loadDataFromStorage();
   // Add functions to delete and to add a book
@@ -60,6 +74,7 @@ const gBookShelf = new BookShelf();
 function displayBooks(bookList) {
   const booksContainer = document.querySelector('.books-container');
   const booksTemplate = document.getElementById('books-template');
+  booksContainer.innerHTML = '';
   bookList.forEach((book) => {
     const bookCard = booksTemplate.content.cloneNode(true).children[0];
     bookCard.querySelector('.title').textContent = book.title;
@@ -74,7 +89,15 @@ function setListeners() {
     if (e.target.matches('.remove-book')) {
       console.log(e.target.dataset.id);
     }
+
+    if (e.target.matches('.add-btn')) {
+      const title = document.getElementById('title').value;
+      const author = document.getElementById('author').value;
+      addBook(title, author, gBookShelf.bookList);
+      displayBooks(gBookShelf.bookList);
+    }
   });
+
 
 }
 
