2a73d8397a0e97204e5097a4eda800149a0d18ce
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-02-07 10:39:04 -0600

    Add the initial BookShelf object and retrieve data from localstorage
    modified:   script.js

--- script.js
+++ script.js
@@ -20,6 +20,42 @@ const bookList = [
     author: 'Test test',
   },
 ];
+function loadDataFromStorage() {
+  let list = JSON.parse(window.localStorage.getItem('bookshelf'));
+  if (!list) {
+    // If it is empty then fill some data to work
+    list = [
+      {
+        id: 0,
+        title: 'Book 1',
+        author: 'Test test',
+      },
+      {
+        id: 1,
+        title: 'Book 2',
+        author: 'Test test',
+      },
+      {
+        id: 2,
+        title: 'Book 3',
+        author: 'Test test',
+      },
+      {
+        id: 3,
+        title: 'Book 4',
+        author: 'Test test',
+      },
+    ];
+  }
+  return list;
+}
+
+function BookShelf() {
+  this.bookList = loadDataFromStorage();
+  // Add functions to delete and to add a book
+}
+
+const gBookShelf = new BookShelf();
 
 function displayBooks(bookList) {
   const booksContainer = document.querySelector('.books-container');
@@ -34,13 +70,13 @@ function displayBooks(bookList) {
 }
 
 function setListeners() {
-  document.addEventListener('click', (e) => { 
-    if(e.target.matches('.remove-book')) {
+  document.addEventListener('click', (e) => {
+    if (e.target.matches('.remove-book')) {
       console.log(e.target.dataset.id);
     }
   });
 
 }
 
-displayBooks(bookList);
+displayBooks(gBookShelf.bookList);
 setListeners();
