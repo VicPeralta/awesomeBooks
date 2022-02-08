2c9e0e3dcdc0f4cb133a8f945bbc40754559fd11
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-02-08 11:44:43 -0600

    Add date/time function
    modified:   index.html
    modified:   script.js
    modified:   styles.css

--- script.js
+++ script.js
@@ -29,6 +29,29 @@ class BookShelf {
   }
 }
 
+function getEndDay(day) {
+  const iDay = Number(day);
+  if (iDay > 3 && iDay < 21) return 'th';
+  switch (iDay % 10) {
+    case 1: return 'st';
+    case 2: return 'nd';
+    case 3: return 'rd';
+    default: return 'th';
+  }
+}
+
+function updateDate() {
+  const dt = new Date();
+  let month = dt.toLocaleString('en-US', { month: 'long' });
+  const day = dt.toLocaleString('en-US', { day: 'numeric' });
+  const endDay = getEndDay(day);
+  const year = dt.toLocaleString('en-US', { year: 'numeric' });
+  const time = dt.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric' });
+  month += ` ${day}${endDay} ${year}, ${time}`;
+  document.querySelector('.datetime p').textContent = month;
+}
+
+window.setInterval(updateDate, 1000);
 const gBookShelf = new BookShelf();
 
 function displayBooks(bookList) {
