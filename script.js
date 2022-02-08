b9642170faec6bc9d30aeea6ed45fdb985b4e3f3
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-02-08 10:55:30 -0600

    Add function to display sections
    I also updated the content of contact section

--- script.js
+++ script.js
@@ -67,7 +67,27 @@ function setListeners() {
       author.value = '';
       displayBooks(gBookShelf.bookList);
     }
+    if (e.target.matches('.navlinks li a')) {
+      const href = e.target.href;
+      const index = href.indexOf('#');
+      const section = href.substring(index);
+      navToSection(e.target, section);
+    }
   });
+}
+
+function navToSection(element, sectionName) {
+  const navLinks = document.querySelectorAll('.navlinks li a');
+  navLinks.forEach((link) => {
+    link.classList.remove('selected')
+  });
+  element.classList.add('selected');
+  const section = document.querySelector(sectionName);
+  const sections = document.querySelectorAll('section');
+  sections.forEach((section) => {
+    section.classList.remove('active');
+  });
+  section.classList.add('active');
 }
 
 displayBooks(gBookShelf.bookList);
